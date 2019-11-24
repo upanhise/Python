12c2bd46a9a3a6d48d484625ab2c65f88ba3f3e9
upanhise <1284657547@qq.com>
2019-11-25 00:29:14 +0800

    浮点数

--- 变量和简单数据模型/数字.py
+++ 变量和简单数据模型/数字.py
@@ -1,7 +1,11 @@
 # 单纯赋值类型为字符串
+# 使用函数str()避免类型错误，str() 值的是返回值为字符串
 age = 23
 message = "Happy " + str(age) + "rd Birthday"
 print(message)
 # 数字之间的加减乘除取决于数的浮点位数
 R = 3.0 / 2
 print(R)
+# 浮点数
+X = 0.1 + 0.1
+print(X)
